84a5da641f89aa0a1e7e99ca9e69d6b03decd7a9
elput0amo <gmiradej@myuax.com>
2023-10-08 12:05:54 +0200

    Update testeo.py

---
+++ testeo.py
@@ -0,0 +1,10 @@
+while True:
+  try:
+    version = input(f"¿Quieres la version facil o complicada?")
+    if version=="Facil" or version=="Complicada":
+      
+      break;
+    else:
+      print(f"{version} no es un comando, aprende a usar mayusculas, porfavor escribe (Izquierda) o (Derecha)")      
+  except:
+    continue
